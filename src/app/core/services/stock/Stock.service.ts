3dcd3658aa77ce5ca0a945f491aaa4a2f6d71f4e
jelassiahmed <ahmed.jelassi@esprit.tn>
2021-11-29 16:58:24 +0100

    Stock List

---
+++ src/app/core/services/stock/Stock.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Stock} from "../../models/stock";
+import {environment} from "../../../../environments/environment";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+
+  url = environment.url + "stock/";
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Stock[]> {
+    return this.http.get<Stock[]>(this.url + "list");
+  }
+
+  add(stock: Stock) {
+    return this.http.post(this.url + "add", stock);
+  }
+
+  update(stock: Stock) {
+    return this.http.put(this.url + "update/" + stock.idStock, stock);
+  }
+
+  delete(id: number) {
+    return this.http.delete(this.url + "delete/" + id);
+  }
+
+  findById(id: number): Observable<Stock> {
+    return this.http.get<Stock>(this.url + "findById/" + id);
+  }
+}
